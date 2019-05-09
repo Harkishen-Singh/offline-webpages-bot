dc67c84d7ab6fe248f1f932cc94af13dcf940755
Harkishen-Singh <harkishensingh@hotmail.com>
2019-05-10 00:59:56 +0530

    resolved syntax errors

--- main_chrome_history_as_json.go
+++ main_chrome_history_as_json.go
@@ -1,31 +1,41 @@
+/**
+*
+* use the following extension to execute the file properly
+* https://chrome.google.com/webstore/detail/history-export/lpmoaclacdaofhlijejogfldmgkdlglj
+*
+*/
+
 package main
 
 import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"net/http"
 	"os"
+	"strings"
+	"os/exec"
 )
 
-//func handlerHistory(url string) {
-//
-//	res, _ := http.Get(url)
-//	baseDirectory := "html_files_dump/_"
-//	url = strings.Replace(url, "https://", "", -1)
-//	url = strings.Replace(url, "http://", "", -1)
-//	resInByteArr, _ := ioutil.ReadAll(res.Body)
-//	resInString := string(resInByteArr)
-//	fmt.Print(resInString)
-//
-//	// creating local storage html files
-//	filePtr, _ := os.Create(baseDirectory + url + "_.html")
-//	_, e := filePtr.Write(resInByteArr)
-//	if e != nil {
-//		panic(e)
-//	}
-//	filePtr.Close()
-//
-//}
+func handlerHistory(url, name string) {
+
+	res, _ := http.Get(url)
+	baseDirectory := "html_files_dump/_"
+	url = strings.Replace(url, "https://", "", -1)
+	url = strings.Replace(url, "http://", "", -1)
+	resInByteArr, _ := ioutil.ReadAll(res.Body)
+	resInString := string(resInByteArr)
+	fmt.Print(resInString)
+
+	// creating local storage html files
+	filePtr, _ := os.Create(baseDirectory + url + "_.html")
+	_, e := filePtr.Write(resInByteArr)
+	if e != nil {
+		panic(e)
+	}
+	filePtr.Close()
+
+}
 
 type chromeHistory struct {
 
@@ -39,8 +49,19 @@ type chromeHistory struct {
 
 }
 
-func importChromeHistoryJSON(name string) {
+func makeDir(name string) bool {
 
+	_, err := exec.Command("mkdir", name).Output()
+	if err != nil {
+		panic(err)
+	}
+	return true
+
+}
+
+func importChromeHistoryJSON(name string) (bool, []chromeHistory) {
+
+	fmt.Println("loading the given History DumpBase ... ")
 	var chromeHistoryArray []chromeHistory
 	file, err := os.Open(name)
 	if err != nil {
@@ -53,12 +74,23 @@ func importChromeHistoryJSON(name string) {
 		panic(err)
 	}
 	fmt.Println(chromeHistoryArray)
+	fmt.Println("done!")
+	return true, chromeHistoryArray
 
 }
 
 func main() {
 
 	arg := os.Args[1]
-	importChromeHistoryJSON(arg)
-
+	if makeDir(arg) {
+		status, chis := importChromeHistoryJSON(arg)
+		lenchis := len(chis)
+		if status {
+			for index, historyOject := range chis {
+				fmt.Print("\tfetching ", index, " out of ", lenchis, " ... ")
+				handlerHistory(historyOject.Url, arg)
+				fmt.Println("done.!")
+			}
+		}
+	}
 }
