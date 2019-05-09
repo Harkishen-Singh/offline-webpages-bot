466a5cb69188b2539d9aca3fc40e60b94db95cb9
Harkishen-Singh <harkishensingh@hotmail.com>
2019-05-09 23:41:31 +0530

    offline web pages based on args

---
+++ main_args.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"os"
+)
+
+func handler(url string) {
+
+	res, _ := http.Get(url)
+	baseDirectory := "html_files_dump/_"
+	url = strings.Replace(url, "https://", "", -1)
+	url = strings.Replace(url, "http://", "", -1)
+	resInByteArr, _ := ioutil.ReadAll(res.Body)
+	resInString := string(resInByteArr)
+	fmt.Print(resInString)
+
+	// creating local storage html files
+	filePtr, _ := os.Create(baseDirectory + url + "_.html")
+	see, e := filePtr.Write(resInByteArr)
+	if e != nil {
+		panic(e)
+	}
+	filePtr.Close()
+
+}
+
+func main() {
+
+	arg := os.Args[1:]
+	fmt.Println(arg)
+	for _, ar := range arg {
+		handler(ar)
+	}
+
+}
